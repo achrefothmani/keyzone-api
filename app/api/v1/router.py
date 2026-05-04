a77a2e4d80f205d3035c889bf38e96f979e2ff1d
Achref Othmani <othmeniachref@gmail.com>
2026-05-04 11:52:40 +0100

    fix: sort api router inclusions alphabetically

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -4,7 +4,7 @@ from app.api.v1.routes import auth, images, properties, stats, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
-api_router.include_router(users.router)
-api_router.include_router(properties.router)
 api_router.include_router(images.router)
+api_router.include_router(properties.router)
 api_router.include_router(stats.router)
+api_router.include_router(users.router)
