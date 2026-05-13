83cc13608e6460d38108201fd0fae719c3506a1f
Achref OTHMANI <othmeniachref@gmail.com>
2026-05-13 14:59:49 +0000

    feat: add websocket connection manager

---
+++ app/core/notifications.py
@@ -0,0 +1,27 @@
+from typing import Dict, List
+from fastapi import WebSocket
+from uuid import UUID
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[UUID, List[WebSocket]] = {}
+
+    async def connect(self, user_id: UUID, websocket: WebSocket):
+        await websocket.accept()
+        if user_id not in self.active_connections:
+            self.active_connections[user_id] = []
+        self.active_connections[user_id].append(websocket)
+
+    def disconnect(self, user_id: UUID, websocket: WebSocket):
+        if user_id in self.active_connections:
+            if websocket in self.active_connections[user_id]:
+                self.active_connections[user_id].remove(websocket)
+            if not self.active_connections[user_id]:
+                del self.active_connections[user_id]
+
+    async def send_personal_message(self, message: dict, user_id: UUID):
+        if user_id in self.active_connections:
+            for connection in self.active_connections[user_id]:
+                await connection.send_json(message)
+
+notification_manager = ConnectionManager()
